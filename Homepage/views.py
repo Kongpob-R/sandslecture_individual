b268b07c72e178c4d92337e4f95dff03cece46fb
Burit Chaisiri <koolkoop2543@gmail.com>
2020-01-29 20:22:51 +0700

    Create template upload and test

--- Homepage/views.py
+++ Homepage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
+def upload(request):
+    return render(request, 'upload.html')
+
--- functional_tests.py
+++ functional_tests.py
@@ -10,26 +10,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
+    def test_home(self):  
          
         # He goes to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # He notices the page title and header mention 
         self.assertIn('S&SLecture', self.browser.title)  
-        
-        # เขาลงชื่อเข้าใช้งานบัญชี โดยการกรอก username กับ password
-        login_button=self.browser.find_element_by_id('login')
-        login_button.send_keys(Keys.ENTER) 
 
-        username = self.browser.find_element_by_id('username')  
-        username.send_keys('Smart')
-
-        password = self.browser.find_element_by_id('password')  
-        password.send_keys('123456')     
-
-        logins_button=self.browser.find_element_by_id('logins')
-        login_button.send_keys(Keys.ENTER) 
 
         # เขาพิมชื่อวิชา "Ubiquitous Computing" ลงไปในกล่องข้อความ
         # เมื่อเขาทำการ enter 
@@ -44,7 +32,29 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')  
 
         # She is invited to enter a to-do item straight away
+
+    def test_login(self):  
+         
+        self.browser.get('http://localhost:8000'+'/accounts/login/')
+        # เขาลงชื่อเข้าใช้งานบัญชี โดยการกรอก username กับ password
+
+
+        username = self.browser.find_element_by_id('id_username')  
+        username.send_keys('Andrew')
+
+        password = self.browser.find_element_by_id('id_password')  
+        password.send_keys('123456')  
         
+        login = self.browser.find_element_by_id('logins')   
+
+        login.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
+        self.fail('Finish the test!')  
+    def upload(self):  
+        self.browser.get(self.test_home+'/upload/')
+        self.fail('Finish the test!')  
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')  
