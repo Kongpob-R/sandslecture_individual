20ef642081ff8a46f36a37bfae5bdb615d2516c7
Therapat <timeza86@gmail.com>
2020-02-03 23:56:13 +0700

    Create Model Profile and forms(Profilepic)

---
+++ Homepage/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import *
+
+# Create the form class.
+class Profileform(ModelForm):
+     class Meta:
+        model = Profile
+        fields = ['Profile_img']
--- Homepage/models.py
+++ Homepage/models.py
@@ -9,3 +9,8 @@ class Lecture(models.Model):
     description = models.CharField(max_length=2000)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank = True, null = True)
     image = models.ImageField(upload_to='lecture_image',blank=True)
+class Profile(models.Model):
+    #User= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    Profile_img=models.ImageField(upload_to="images/")
+    #Lecture_me = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    objects=models.Manager()
